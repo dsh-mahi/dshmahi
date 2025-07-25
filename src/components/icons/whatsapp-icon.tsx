53d4695b9e9b153589b9001f0d16d7967bc13652
DEWAN SAHARIAR HOSSAIN MAHI 0242220005291030 <mahi22205291030@diu.edu.bd>
2025-07-25 08:13:42 +0000

    fix the whastapp icon

--- src/components/icons/whatsapp-icon.tsx
+++ src/components/icons/whatsapp-icon.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 const WhatsAppIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
+    <svg
     xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
@@ -14,7 +14,7 @@ const WhatsAppIcon = (props: React.SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-    <path d="M14.05 2.94A10.03 10.03 0 0 0 4.97 12.63m14.06-2.58A10.03 10.03 0 0 0 11.37 4.97" />
+    <path d="M19.37 4.97a9.99 9.99 0 0 0-14.4 0m14.4 0a9.99 9.99 0 0 1 0 14.06m-14.4 0a9.99 9.99 0 0 1 0-14.06" />
   </svg>
 );
 
